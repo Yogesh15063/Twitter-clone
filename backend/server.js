825427df4cb990e2a84ed02b728c7a3f313b35b3
Yogesh15063 <yj15063@gmail.com>
2024-09-18 16:31:45 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import userRoutes from "./routes/user.routes.js";
 import postRoutes from './routes/post.routes.js'
 import notificationRoutes from './routes/notification.routes.js'
 import connectMongoDB from "./db/connectMongodb.js";
-
+import path from 'path'
 
 dotenv.config();
 cloudinary.config({
@@ -18,7 +18,7 @@ cloudinary.config({
 })
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve()
 // Middleware to parse incoming request bodies
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({ extended: true }));
@@ -28,6 +28,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if(process.env.NODE_ENV ==="production"){
+  app.use(express.static(path.join(__dirname,"/frontend/dist")));
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend","dist", "index.html"));
+  })
+}
 // Connect to MongoDB and start the server
 connectMongoDB()
   .then(() => {
